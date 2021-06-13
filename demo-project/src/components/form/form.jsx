5bf1da4bd32ec1aa6604fecb1ce0b483cf5b1ebe
igoradamenko <mail@igoradamenko.com>
2021-06-13 11:56:35 +0300

    Pass onSubmit to Form

--- demo-project/src/components/form/form.jsx
+++ demo-project/src/components/form/form.jsx
@@ -1,9 +1,13 @@
 export default function Form(props) {
   return (
-    <form className="form">{props.children}</form>
+    <form
+      className="form"
+      onSubmit={props.onSubmit}
+    >{props.children}</form>
   );
 }
 
 Form.propTypes = {
   children: PropTypes.node,
+  onSubmit: PropTypes.func,
 };
