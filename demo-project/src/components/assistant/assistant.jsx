be4c05144371b972914f5f6ccc860656422abe4e
igoradamenko <mail@igoradamenko.com>
2021-06-13 20:04:35 +0300

    Refactor Assistant block

--- demo-project/src/components/assistant/assistant.jsx
+++ demo-project/src/components/assistant/assistant.jsx
@@ -1,61 +1,72 @@
+const DATA_BY_TYPE = {
+  success: {
+    photoDescription: 'Winking Tony Stark',
+    photoSrc1x: require('./_type/_success/assistant_type_success.png'),
+    photoSrc2x: require('./_type/_success/assistant_type_success@2x.png'),
+  },
+  greeting: {
+    photoDescription: 'Smiling Tony Stark',
+    photoSrc1x: require('./_type/_greeting/assistant_type_greeting.png'),
+    photoSrc2x: require('./_type/_greeting/assistant_type_greeting@2x.png'),
+    text: (
+      <>
+        <p>Hey kid!</p>
+        <p>Hope you haven&rsquo;t forgotten the password you set back then in&nbsp;Avengers tower!</p>
+      </>
+    ),
+  },
+  'password-reset': {
+    photoDescription: 'Rolling eyes Tony Stark',
+    photoSrc1x: require('./_type/_password-reset/assistant_type_password-reset.png'),
+    photoSrc2x: require('./_type/_password-reset/assistant_type_password-reset@2x.png'),
+    text: (
+      <>
+        <p>Well, that&rsquo;s pretty.</p>
+        <p>I&rsquo;m giving full access to&nbsp;the system that may kill everyone on&nbsp;Earth to&nbsp;the boy who can&rsquo;t even remember his own password. Nice!</p>
+      </>
+    ),
+  }
+}
+
+function getSrcSet(data) {
+  return `${data.photoSrc1x} 1x, ${data.photoSrc2x} 2x`;
+}
+
 export default function Assistant(props) {
   const type = props.mods.type;
+  const data = DATA_BY_TYPE[type];
+
+  if (!data) {
+    throw new Error('Unknown Assistant type');
+  }
 
   if (type === 'success') {
     return (
       <div className={b('assistant', props)}>
         <img
-          src={require('./_type/_success/assistant_type_success.png')}
-          srcSet={`${require('./_type/_success/assistant_type_success.png')} 1x, ${require('./_type/_success/assistant_type_success@2x.png')} 2x`}
-          alt="Winking Tony Stark"
-          className="assistant__logo"
+          src={data.photoSrc1x}
+          srcSet={getSrcSet(data)}
+          alt={data.photoDescription}
+          className="assistant__photo"
         />
       </div>
     );
   }
 
-  if (type === 'greeting' || type === 'password-reset') {
-    const photoSrc = type === 'greeting' 
-      ? require('./_type/_greeting/assistant_type_greeting.png')
-      : require('./_type/_password-reset/assistant_type_password-reset.png');
-
-    const photoSrc2x = type === 'greeting' 
-      ? require('./_type/_greeting/assistant_type_greeting@2x.png')
-      : require('./_type/_password-reset/assistant_type_password-reset@2x.png');
-
-    const photoDesc = type === 'greeting'
-      ? 'Smiling Tony Stark'
-      : 'Rolling eyes Tony Stark';
-
-    return (
-      <div className={b('assistant', props)}>
-        <div className="assistant__message">
-          <img 
-            src={photoSrc} 
-            srcSet={`${photoSrc} 1x, ${photoSrc2x} 2x`}
-            alt={photoDesc}
-            className="assistant__logo"
-          />
-          
-          {type === 'greeting' && (
-            <>
-              <p>Hey kid!</p>
-              <p>Hope you haven&rsquo;t forgotten the password you set back then in&nbsp;Avengers tower!</p>
-            </>
-          )}
-
-          {type === 'password-reset' && (
-            <>
-              <p>Well, that&rsquo;s pretty.</p>
-              <p>I&rsquo;m giving full access to&nbsp;the system that may kill everyone on&nbsp;Earth to&nbsp;the boy who can&rsquo;t even remember his own password. Nice!</p>
-            </>
-          )}
-        </div>
+  return (
+    <div className={b('assistant', props)}>
+      <div className="assistant__message">
+        <img 
+          src={data.photoSrc1x} 
+          srcSet={getSrcSet(data)}
+          alt={data.photoDescription}
+          className="assistant__photo assistant__photo_view_floating"
+        />
+        
+        {data.text}
       </div>
-    );
-  }
-
-  throw new Error('Unknown Assistant type');
+    </div>
+  );
 }
 
 // TODO: check proptypes
--- demo-project/src/components/assistant/index.js
+++ demo-project/src/components/assistant/index.js
@@ -1,8 +1,8 @@
 export { default } from './assistant';
 
-require('./__logo/assistant__logo.scss');
+require('./__photo/assistant__photo.scss');
+require('./__photo/_view/_floating/assistant__photo_view_floating.scss');
+
 require('./__message/assistant__message.scss');
 
-require('./_type/_greeting/assistant_type_greeting.scss');
-require('./_type/_password-reset/assistant_type_password-reset.scss');
 require('./_type/_success/assistant_type_success.scss');
