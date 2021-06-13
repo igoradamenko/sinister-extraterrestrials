a9088cff4b88c3fdab08f29a15409047d3ec0201
igoradamenko <mail@igoradamenko.com>
2021-06-13 11:58:43 +0300

    Add dummy auth post in loading view

--- demo-project/src/views/login-view/login-view.jsx
+++ demo-project/src/views/login-view/login-view.jsx
@@ -3,6 +3,8 @@ import Button from 'components/button';
 import Form, { Form__Field, Form__Label } from 'components/form';
 import Input from 'components/input';
 
+import { STATUS, postAuth } from 'services/fetcher';
+
 export default class LoginView extends Component {
   constructor(props) {
     super(props);
@@ -10,27 +12,43 @@ export default class LoginView extends Component {
     this.state = {
       email: '',
       password: '',
+      isLoading: false,
+      isLoggedIn: false,
+      isError: false,
     };
 
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleEmailChange(email) {
-    this.setState({ email });
+    this.setState({ email, isError: false });
   }
 
   handlePasswordChange(password) {
-    this.setState({ password });
+    this.setState({ password, isError: false });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const { email, password } = this.state;
+
+    this.setState({ isLoading: true });
+
+    postAuth({ email, password })
+      .then(() => this.setState({ isLoading: false, isLoggedIn: true }))
+      .catch(() => this.setState({ isLoading: false, isError: true }));
   }
 
   render() {
-    const { email, password } = this.state;
+    const { email, password, isLoading } = this.state;
 
     return (
     // TODO: add face id login
     // TODO: errors!
-    <Form>
+    <Form onSubmit={this.handleSubmit}>
       <Assistant mix="form__assistant"/>
 
       <Form__Field>
@@ -38,7 +56,7 @@ export default class LoginView extends Component {
         <Form__Label>E-mail:</Form__Label>
         
         <Input
-          mods={{ type: 'email' }}
+          mods={{ type: 'email', disabled: isLoading }}
           mix="form__input"
           value={email}
           onChange={this.handleEmailChange}
@@ -51,7 +69,7 @@ export default class LoginView extends Component {
         
         {/* TODO: add a way to make password visible */}
         <Input
-          mods={{ type: 'password' }}
+          mods={{ type: 'password', disabled: isLoading }}
           mix="form__input" 
           value={password}
           onChange={this.handlePasswordChange}
@@ -59,13 +77,14 @@ export default class LoginView extends Component {
       </Form__Field>
 
       <Form__Field>
+        {/* TODO: block if not all the fields are filled */}
         <Button
-          mods={{ type: 'submit', view: 'standard' }}
+          mods={{ type: 'submit', view: 'standard', loading: isLoading }}
           mix="form__action"
         >Sign in</Button>
         
         <Button
-          mods={{ type: 'button', view: 'pseudo-link' }}
+          mods={{ type: 'button', view: 'pseudo-link', disabled: isLoading }}
           mix="form__action"
         >Reset password</Button>
       </Form__Field>
