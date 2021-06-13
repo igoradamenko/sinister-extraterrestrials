ae56ab5414100665adcdaa1129b324f5edbc396f
igoradamenko <mail@igoradamenko.com>
2021-06-13 14:24:08 +0300

    Add password-reset assistant type

--- demo-project/src/components/assistant/assistant.jsx
+++ demo-project/src/components/assistant/assistant.jsx
@@ -1,18 +1,6 @@
 export default function Assistant(props) {
   const type = props.mods.type;
 
-  if (type === 'greeting') {
-    return (
-      <div className={b('assistant', props)}>
-        {/* TODO: a11y; also optimize */}
-        <img src={require('./_type/_greeting/assistant_type_greeting.png')} alt="" className="assistant__logo"/>
-        
-        <p>Hey kid!</p>
-        <p>Hope you haven't forgotten the password you set back then in Avengers tower!</p>
-      </div>
-    );
-  }
-
   if (type === 'success') {
     return (
       <div className={b('assistant', props)}>
@@ -22,11 +10,46 @@ export default function Assistant(props) {
     );
   }
 
+  if (type === 'greeting' || type === 'password-reset') {
+    const photoSrc = type === 'greeting' 
+      ? require('./_type/_greeting/assistant_type_greeting.png')
+      : require('./_type/_password-reset/assistant_type_password-reset.png')
+
+    return (
+      <div className={b('assistant', props)}>
+        <div className="assistant__message">
+          {/* TODO: a11y; also optimize */}
+          <img 
+            src={photoSrc} 
+            alt="" 
+            className="assistant__logo"
+          />
+          
+          {/* TODO: typograf */}
+          {type === 'greeting' && (
+            <>
+              <p>Hey kid!</p>
+              <p>Hope you haven't forgotten the password you set back then in Avengers tower!</p>
+            </>
+          )}
+
+          {type === 'password-reset' && (
+            <>
+              <p>Well, that's pretty.</p>
+              <p>I'm giving full access to the system that may kill everyone on Earth to the boy who can't even remember his own password. Nice!</p>
+            </>
+          )}
+          
+        </div>
+      </div>
+    );
+  }
+
   throw new Error('Unknown Assistant type');
 }
 
 Assistant.propTypes = {
   mods: PropTypes.shape({
-    type: PropTypes.oneOf(['greeting', 'success']).isRequired,
+    type: PropTypes.oneOf(['greeting', 'success', 'password-reset']).isRequired,
   }).isRequired,
 };
--- demo-project/src/components/assistant/index.js
+++ demo-project/src/components/assistant/index.js
@@ -3,6 +3,8 @@ export { default } from './assistant';
 require('./assistant.scss');
 
 require('./__logo/assistant__logo.scss');
+require('./__message/assistant__message.scss');
 
 require('./_type/_greeting/assistant_type_greeting.scss');
+require('./_type/_password-reset/assistant_type_password-reset.scss');
 require('./_type/_success/assistant_type_success.scss');
